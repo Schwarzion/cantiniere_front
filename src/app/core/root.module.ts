d1d71a07762c0c4b6d2dbecceeb2be7741b39be3
redha <redha@ax2lan.com>
2019-11-29 16:47:08 +0100

    home and menu first commit

--- src/app/core/root.module.ts
+++ src/app/core/root.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 import * as base from './app/components/index';
 
 import { RootRoutingModule } from './root-routing.module';
