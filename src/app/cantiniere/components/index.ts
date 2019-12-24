deafa8d1fe36ddc1fa82e5840021757c70fdfa65
Lucas Poirier <lucas@tymate.com>
2019-12-24 15:06:43 +0100

    adding order component

--- src/app/cantiniere/components/index.ts
+++ src/app/cantiniere/components/index.ts
@@ -1,5 +1,7 @@
 import { OrdersListComponent } from './orders-list/orders-list.component';
+import { OrderComponent } from './order/order.component';
 
-export const component = [OrdersListComponent];
+export const component = [OrdersListComponent, OrderComponent];
 
 export * from './orders-list/orders-list.component';
+export * from './order/order.component';
