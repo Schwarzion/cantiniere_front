372efcc5013539178109f85cf0fca3ff45bf233b
Lucas Poirier <lucas@tymate.com>
2020-01-06 19:05:25 +0100

    feat: addd pictures for meals, ingredients, and menus, user can now put an order with specified quantities

--- src/app/cantiniere/cantiniere-routing.module.ts
+++ src/app/cantiniere/cantiniere-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { OrdersListComponent } from './components';
+import { IngredientsComponent } from './components/ingredients/ingredients.component';
 
 const routes: Routes = [
   { path: '', component: OrdersListComponent },
+  { path: 'ingredients', component:IngredientsComponent },
   // Redirection en cas de route non trouvée
   {path: '**', component: OrdersListComponent}
 ];
--- src/app/cantiniere/cantiniere.module.ts
+++ src/app/cantiniere/cantiniere.module.ts
@@ -4,9 +4,10 @@ import { SharedModule } from '../shared/shared.module';
 import { CantiniereRoutingModule } from './cantiniere-routing.module';
 
 import * as cantiniere from './components/index';
+import { IngredientsComponent } from './components/ingredients/ingredients.component';
 
 @NgModule({
-  declarations: [...cantiniere.component],
+  declarations: [...cantiniere.component, IngredientsComponent],
   imports: [CommonModule, SharedModule, CantiniereRoutingModule, SharedModule],
 })
 export class CantiniereModule {}
