b168e8e07b19b154e51c8d5ec3691027742b1491
AnthonySar <anthonyanthony.sarapata@gmail.com>
2020-01-19 19:37:40 +0100

    Mauvaise version, revenir commit antérieure

---
+++ src/app/core/authguards/lunch-lady-guard.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { UserService } from 'src/app/services/user.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LunchLadyGuard implements CanActivate {
+  constructor (private userService: UserService, private router: Router) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (this.userService.isLunchLady()) {
+      return true;
+    } else {
+      this.router.navigate(['/']);
+    }
+  }
+
+}
