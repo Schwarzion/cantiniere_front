3c37aabef0dd29fdd68da507ac548dffa7703cf2
Lucas Poirier <lucas@tymate.com>
2020-01-26 20:07:42 +0100

    fix: footer height

--- src/app/core/root.component.ts
+++ src/app/core/root.component.ts
@@ -4,7 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <app-header></app-header>
+    <div style="min-height: 75vh;">
     <router-outlet></router-outlet>
+    </div>
     <app-footer></app-footer>
   `,
 })
