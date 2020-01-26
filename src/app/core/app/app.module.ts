ba959ed7618672ac60cd2af7dce37e2002c7bed9
fadika.mohamed02 <fadika.mohamed02@gmail.com>
2020-01-26 03:49:24 +0100

    Login modal customization, done

--- src/app/core/app/app.module.ts
+++ src/app/core/app/app.module.ts
@@ -7,10 +7,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './components/login/login.component';
 import { MatDialogModule, MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [AppComponent, base.HomeComponent],
-  imports: [CommonModule, AppRoutingModule, HttpClientModule, SharedModule, MatDialogModule],
+  imports: [CommonModule, AppRoutingModule, HttpClientModule, SharedModule, RouterModule],
   providers: [],
   bootstrap: [AppComponent],
 })
